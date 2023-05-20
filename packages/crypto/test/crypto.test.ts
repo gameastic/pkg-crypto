6b4e5f21b91e42753ea369a46972d6dac7d0e349
Ashot Avagyan <avagyanah@gmail.com>
2023-05-21 00:28:31 +0400

    added tests

--- packages/crypto/test/crypto.test.ts
+++ packages/crypto/test/crypto.test.ts
@@ -1,3 +1,18 @@
-test('crypto', () => {
-    //
+import { Crypto } from '../src/crypto';
+
+const data = 'Lorem Ipsum';
+
+test('crypto with secret', () => {
+    const secret = 'G;MQ)Q<S0)q[>GoHtMgIOn8<1Sn+@T';
+    const encrypted = Crypto.encrypt(data, secret);
+    const decrypted = Crypto.decrypt(encrypted, secret);
+
+    expect(decrypted).toEqual(data);
+});
+
+test('crypto without secret', () => {
+    const encrypted = Crypto.encrypt(data);
+    const decrypted = Crypto.decrypt(encrypted);
+
+    expect(decrypted).toEqual(data);
 });
